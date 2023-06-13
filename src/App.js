be8f397f2d3e9422a9128ca57c2862224f4e0066
friska711 <126258596+friska711@users.noreply.github.com>
2023-06-13 19:42:17 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
-import logo from './logo.svg'
+
 import './App.css';
-import LuasSegitiga from './components/LuasSegitiga';
-import  LuasLingkaran  from './components/LuasLingkaran';
-import { useState } from 'react';
-import { Counter } from './components/counter';
-import { CounterProvider } from './context/CounterProvider';
-import { CounterTwoNumber } from './components/CounterTwoNumber';
-import { FormText } from './components/FormText';
-import { Nilai } from './components/Nilai';
-
+import { ButtonGoogleLogin } from './components/ButtonGoogleLogin';
 function App() {
-  const [page, setPage] = useState ('luasSegitiga')
-
   return (
-    <div className='App'>
-      <CounterProvider>
-        <button onClick={() => setPage ('luasSegitiga')}>Luas Segitiga</button>
-        <button onClick={() => setPage ('luasLingkaran')}>Luas Lingkaran</button>
-        {
-          page === 'luasSegitiga' ? <LuasSegitiga/> : <LuasLingkaran/>
-        }
-        <Counter/>
-      </CounterProvider>
-      <CounterTwoNumber/>
-      <FormText/>
-      <Nilai/>
+    <div className="App">
+      <ButtonGoogleLogin/>
     </div>
   );
 }
+
 export default App;
